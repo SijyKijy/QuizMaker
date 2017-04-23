07c37f81d85daba4035682185c8f0c3b8a2cf9b6
SijyKijy <max.monovcov@gmail.com>
2017-04-23 19:05:43 +0300

    123

--- QuizMaker/Form1.Designer.cs
+++ QuizMaker/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 538);
+            this.ClientSize = new System.Drawing.Size(627, 421);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
